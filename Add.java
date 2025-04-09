15cc1e1fc44413b3a88823775f4149e644670b2e
arun123-max <arunp7423@gmail.com>
2025-04-09 14:24:13 +0530

    add

--- Add.java
+++ Add.java
@@ -3,7 +3,7 @@ public class Add
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
-        int sum = a + b;
+        int sum = a - b;
         System.out.println("sum is " + sum);
     }
 }
